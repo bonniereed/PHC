d40c2fccc48a91078c39dc7148d1f640b6919af9
BonWork <BonWork@Bonnies-MacBook-Pro-2.local>
2022-09-26 21:23:24 -0500

    adding completed code challenge

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
-# PHC
-Code Challenge
+# Parkhub nodeJS Challenge
+
+# Description
+This is an example of a RESTFUL API that processes data from the Santa Monica Parking API with a single endpoint.
+
+This endpoint processes a zipcode and returns the following data:
+
+An array of lots with the following data for each lot
+Available spaces in each lot
+Name for each lot
+Steet Address
+An array of meters with the following data for each meter:
+"active" status
+meter ID
+How many active meters there are in that zip code
+How many lots have available spaces in that zip code
+
+This is an example of the expected return:
+```JSON
+{
+    "90401": {
+        "lots": [
+            //array of lots returned based on zipcode selection
+        ],
+        "meters": [
+            //array of meters returned based on zipcode selection
+        ]
+    }
+}
+```
+
+# Usage:
+
+* Open this zipped folder in VScode. 
+* Right click on the 'index.js' file and select 'Open in integrated terminal'.
+* In the terminal, type  ```npm i```.
+* To run the server, type ```node .```
+* Open a web browser and navigate to localhost:3001/api/(Zipcode of your choice).
+
---
+++ index.js
@@ -0,0 +1,71 @@
+//importing dependencies
+const express = require("express");
+const app = express();
+const PORT = 3001;
+const { default: axios } = require("axios");
+const apiUrlLots = "https://parking.api.smgov.net/lots";
+const apiUrlMeters = "https://parking.api.smgov.net/meters";
+const { geo2zip } = require("geo2zip");
+//config for request header
+const config = [{ method: "GET", headers: { Accept: "application/json" } }];
+//returned lot info will push to this array
+let lotArr = [];
+//returned meter info will push to this array
+let meterArr = [];
+//server listening on port 3001
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+//retrieving lot info
+const getLotArr = axios.get(apiUrlLots, [config]).then((response) => {
+  const returnedSpaces = response.data;
+  //pushing data to lot array for later use
+  returnedSpaces.forEach((returnedSpace) => {
+    lotArr.push({
+      available_spaces: returnedSpace.available_spaces,
+      name: returnedSpace.name,
+      street_address: returnedSpace.street_address,
+      zip_code: returnedSpace.zip_code,
+    });
+  });
+});
+//retrieving meter info
+const getMeterArr = axios.get(apiUrlMeters, [config]).then((response) => {
+  const returnedMeters = response.data;
+  //pushing to meter array for later use
+  returnedMeters.forEach((returnedMeter) => {
+    meterArr.push({
+      active: returnedMeter.active,
+      meter_id: returnedMeter.meter_id,
+      latitude: returnedMeter.latitude,
+      longitude: returnedMeter.longitude,
+    });
+  });
+});
+
+//returning direction for user 
+app.get("/", async (req, res) => {
+  res.json('Please navigate to localhost:3001/api/(yourZipCodeHere)');
+});
+//returning lots by zip and meters by zip
+app.get("/api/:zip_code", async (req, res) => {
+  let zipRequest = req.params.zip_code;
+  getMeterArr;
+  getLotArr;
+  let meterZipArr = [];
+  let newArr = await meterArr.map(async (meter) => {
+    let zipResponse = await geo2zip({
+      latitude: meter.latitude,
+      longitude: meter.longitude,
+    });
+    if (zipResponse == zipRequest) {
+      meterZipArr.push(meter);
+    }
+  });
+  let lotZipArr = lotArr.filter((lot) => lot.zip_code == zipRequest);
+  const responseData = {
+    [zipRequest]: {
+      lots: lotZipArr,
+      meters: meterZipArr,
+    },
+  };
+  res.json(responseData);
+});
